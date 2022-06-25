3db122c066daaba2f4218328f18fa3cbc63c5bc4
rifkytecoder <rifkytecoder@gmail.com>
2022-06-25 11:18:05 +0800

    secret key

--- payment/service.go
+++ payment/service.go
@@ -20,8 +20,8 @@ func NewService() *service {
 
 func (s *service) GetPaymentURL(transaction Transaction, user user.User) (string, error) {
 	midclient := midtrans.NewClient()
-	midclient.ServerKey = "SB-Mid-server-jF76yh7Dj1u47w2uA2TV6u01" //"YOUR-VT-SERVER-KEY"
-	midclient.ClientKey = "SB-Mid-client-UUGRfsIHH34cUgqy"         //"YOUR-VT-CLIENT-KEY"
+	midclient.ServerKey = "" //"YOUR-VT-SERVER-KEY"
+	midclient.ClientKey = "" //"YOUR-VT-CLIENT-KEY"
 	midclient.APIEnvType = midtrans.Sandbox
 
 	snapGateway := midtrans.SnapGateway{
